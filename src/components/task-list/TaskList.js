0124416bbd33671c03845509a3740330664d4065
gekasam <gekasam94@gmail.com>
2023-12-25 00:12:33 +0300

    TodoApp#1.0

--- src/components/task-list/TaskList.js
+++ src/components/task-list/TaskList.js
@@ -3,11 +3,11 @@ import Task from '../task';
 export default function TaskList({ data }) {
   const elements = data.map((element) =>
     element.class !== 'editing' ? (
-      <li className={element.class} key={element.value + new Date().getTime()}>
+      <li className={element.class} key={element.value + Date.now()}>
         <Task value={element.value} />
       </li>
     ) : (
-      <li className={element.class} key={element.value + new Date().getTime()}>
+      <li className={element.class} key={element.value + Date.now()}>
         <Task value={element.value} />
         <input type="text" className="edit" value={element.value} />
       </li>
