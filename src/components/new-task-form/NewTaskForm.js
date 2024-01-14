703e3af27aec8fbc6fe5dda30e877cf6ca703d85
gekasam <gekasam94@gmail.com>
2024-01-14 22:40:31 +0300

    TodoApp NewTaskForm to hooks

--- src/components/new-task-form/NewTaskForm.js
+++ src/components/new-task-form/NewTaskForm.js
@@ -1,99 +1,74 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export default class NewTaskForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      inputNewTask: '',
-      inputMinutes: '',
-      inputSeconds: '',
-    };
+export default function NewTaskForm({ onAddTask }) {
+  const [inputNewTask, setInputNewTask] = useState('');
+  const [inputMinutes, setInputMinutes] = useState('');
+  const [inputSeconds, setInputSeconds] = useState('');
 
-    this.handlerNewTask = this.handlerNewTask.bind(this);
-    this.handlerMin = this.handlerMin.bind(this);
-    this.handlerSec = this.handlerSec.bind(this);
-    this.handlerKeyDown = this.handlerKeyDown.bind(this);
-    this.handlerSubmit = this.handlerSubmit.bind(this);
+  function handlerNewTask(e) {
+    setInputNewTask(e.target.value);
   }
 
-  handlerNewTask(e) {
-    this.setState({
-      inputNewTask: e.target.value,
-    });
-  }
-
-  handlerMin(e) {
+  function handlerMin(e) {
     if (e.target.value.match(/^\d*$/) && e.target.value <= 60) {
-      this.setState({
-        inputMinutes: e.target.value,
-      });
+      setInputMinutes(e.target.value);
     }
   }
 
-  handlerSec(e) {
+  function handlerSec(e) {
     if (e.target.value.match(/^\d*$/) && e.target.value <= 60) {
-      this.setState({
-        inputSeconds: e.target.value,
-      });
+      setInputSeconds(e.target.value);
     }
   }
 
-  handlerKeyDown(e) {
-    if (e.key === 'Enter') {
-      this.handlerSubmit();
-    }
-  }
-
-  handlerSubmit() {
-    const { inputNewTask, inputMinutes, inputSeconds } = this.state;
-    const { onAddTask } = this.props;
-
+  function handlerSubmit() {
     const timeInSeconds = inputMinutes * 60 + +inputSeconds;
 
     if (inputNewTask && (inputMinutes > 0 || inputSeconds > 0)) {
-      this.setState({
-        inputNewTask: '',
-        inputMinutes: '',
-        inputSeconds: '',
-      });
+      setInputNewTask('');
+      setInputMinutes('');
+      setInputSeconds('');
       onAddTask(inputNewTask, timeInSeconds);
     }
   }
 
-  render() {
-    const { inputNewTask, inputMinutes, inputSeconds } = this.state;
-    return (
-      <form className="new-todo-form">
-        <input
-          name="new-task-input"
-          className="new-todo"
-          placeholder="What needs to be done?"
-          value={inputNewTask}
-          onChange={this.handlerNewTask}
-          onKeyDown={this.handlerKeyDown}
-          // eslint-disable-next-line jsx-a11y/no-autofocus
-          autoFocus
-        />
-        <input
-          name="min-input"
-          className="new-todo-form__timer"
-          placeholder="Min"
-          value={inputMinutes}
-          onChange={this.handlerMin}
-          onKeyDown={this.handlerKeyDown}
-        />
-        <input
-          name="sec-input"
-          className="new-todo-form__timer"
-          placeholder="Sec"
-          value={inputSeconds}
-          onChange={this.handlerSec}
-          onKeyDown={this.handlerKeyDown}
-        />
-      </form>
-    );
+  function handlerKeyDown(e) {
+    if (e.key === 'Enter') {
+      handlerSubmit();
+    }
   }
+
+  return (
+    <form className="new-todo-form">
+      <input
+        name="new-task-input"
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={inputNewTask}
+        onChange={handlerNewTask}
+        onKeyDown={handlerKeyDown}
+        // eslint-disable-next-line jsx-a11y/no-autofocus
+        autoFocus
+      />
+      <input
+        name="min-input"
+        className="new-todo-form__timer"
+        placeholder="Min"
+        value={inputMinutes}
+        onChange={handlerMin}
+        onKeyDown={handlerKeyDown}
+      />
+      <input
+        name="sec-input"
+        className="new-todo-form__timer"
+        placeholder="Sec"
+        value={inputSeconds}
+        onChange={handlerSec}
+        onKeyDown={handlerKeyDown}
+      />
+    </form>
+  );
 }
 
 NewTaskForm.propTypes = {
